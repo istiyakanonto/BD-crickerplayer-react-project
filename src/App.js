9b156f97f685f72f28604880f45f4002bfad7869
istiyakanonto <istiyak.anonto@gmail.com>
2021-03-01 00:32:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import playersData from './FakeData/data.json'
 
 function App() {
+  console.log(playersData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ul>
+       {
+         playersData.map(data=> <li>{data.first_name}</li>)
+       }
+     </ul>
     </div>
   );
 }
