7a4b089f1ffa09cfc4cfa9eee7977f9e17f8f9c9
istiyakanonto <istiyak.anonto@gmail.com>
2021-03-01 20:40:22 +0600

    finalOne

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ import Result from './component/Result/Result';
 function App() {
   const first10 = playersData.slice(0, 10)
   const [playersInfo, setPlayersInfo] = useState(first10)
-  const [addPlayer, setAddPlayer]=useState([])
-  const handleAddPlayer=(player)=>
-  {
-    const newPlayer=[...addPlayer,player]
-  setAddPlayer(newPlayer)
+  const [addPlayer, setAddPlayer] = useState([])
+  const handleAddPlayer = (player) => {
+    const newPlayer = [...addPlayer, player]
+    setAddPlayer(newPlayer)
   }
   return (
     <div className="result ">
- <div className='show-result'>
-    <Result addPlayer={addPlayer}></Result>
-     </div>
-<div className='player-info'>
-{
-           playersInfo.map(play=><Player handleAddPlayer={handleAddPlayer} player={play}></Player>)
-      }
-</div>
-</div>
-)
+      <div className='show-result'>
+        <Result addPlayer={addPlayer}></Result>
+      </div>
+      <div className='player-info'>
+        {
+          playersInfo.map(play => <Player handleAddPlayer={handleAddPlayer} player={play}></Player>)
+        }
+      </div>
+    </div>
+  )
 }
 export default App;
